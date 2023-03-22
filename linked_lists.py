2be9c245d6a50bdd654b4a20c9c01099635b7770
KingBubIII <kingbubiii@pop-os.localdomain>
2023-03-21 21:23:21 -0600

    linked list class

---
+++ linked_lists.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value) -> None:
+        self.head = {"value" : value, "next" : None}
+        self.tail = self.head
+        self.length = 1
+
+    def printList(self):
+        currNode = self.head
+        myArr = []
+        while not currNode is None:
+            myArr.append(currNode["value"])
+            currNode = currNode["next"]
+        print(myArr)
+
+    def Traverse(self, target):
+        index = 0
+        currNode = self.head
+
+        while not index == target:
+            currNode = currNode["next"]
+            index += 1
+
+        return currNode
+    
+    def append(self, value):
+        newNode = {"value" : value, "next" : None}
+        self.tail["next"] = newNode
+        self.tail = newNode
+        self.length += 1
+    
+    def prepend(self, value):
+        newNode = {"value" : value, "next" : None}
+        newNode["next"] = self.head
+        self.head = newNode
+        self.length += 1
+
+    def insert(self, targetI, value):        
+        newNode = {"value" : value, "next" : None}
+        pre = self.Traverse(targetI-1)
+
+        newNode["next"] = pre["next"]
+        pre["next"] = newNode
+        
+        self.length += 1
+        return
+
+    def remove(self, index):
+        pre = self.Traverse(index-1)
+        pre["next"] = pre["next"]["next"]
+        self.length -= 1
+        return
+    
+
+
+myll = LinkedList(10)
+
+myll.append(5)
+myll.append(16)
+myll.prepend(1)
+myll.insert(2,99)
+myll.remove(2)
+myll.remove(3)
+myll.printList()
