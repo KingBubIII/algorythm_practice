fe41c8b9923a7589f1d93ec249613e7a1b032fec
Caleb Richardson <richardsoncaleb60@gmail.com>
2023-06-29 23:26:31 -0600

    new quick sort with less space complexity

--- alrgorythms/sorting/quick_sort.py
+++ alrgorythms/sorting/quick_sort.py
@@ -30,3 +30,36 @@ def quickSort(array):
 # Select first and last index as 2nd and 3rd parameters
 result = quickSort(numbers)
 print(result)
+
+numbers = [0, 99, 6, 2, 1, 5, 63, 87, 283, 4, 44]
+
+def quickSort2(array, left, right):
+    pivot_index = right
+    curr_change = left-1
+    checking = left
+    
+    if len(array[left:right+1])==0:
+        return []
+    elif len(array[left:right+1])==1:
+        return array[left:right+1]
+    else:
+        while array[checking] != array[pivot_index]:
+            if array[checking] < array[pivot_index]:
+                curr_change+=1
+                temp = array[curr_change]
+                array[curr_change] = array[checking]
+                array[checking] = temp
+            checking+=1
+        
+        curr_change+=1
+        temp = array[curr_change]
+        array[curr_change] = array[checking]
+        pivot_index = curr_change
+        array[checking] = temp
+
+        array = quickSort2(array,left,pivot_index-1) + [array[pivot_index]] + quickSort2(array, pivot_index+1, checking)
+        return array
+
+# Select first and last index as 2nd and 3rd parameters
+result = quickSort2(numbers,0, len(numbers)-1)
+print(result)
